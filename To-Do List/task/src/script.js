2486e86faedd516c2b2e2d9e0e0856bc071c6037
viacheslavorlov <viacheslavorlov@gmail.com>
2022-06-03 00:36:05 +0300

    Creating JB-TODO APP on GitHub

---
+++ To-Do List/task/src/script.js
@@ -0,0 +1,58 @@
+const inputField = document.getElementById('input-task');
+const addBtn = document.getElementById('add-task-button');
+const ul = document.getElementById('task-list');
+
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+
+
+function Task(description) {
+	this.description = description;
+	this.completed = false;
+}
+
+const updateLocalStorage = function () {
+	if (taskList.length > 0) {
+		localStorage.setItem('tasks', JSON.stringify(taskList));
+	}
+}
+
+function createNewTask(value) {
+	if (value.length > 0) {
+		let taskField = document.createElement('li')
+		taskField.innerHTML = `
+				<input type="checkbox">
+	            <span class="task">${value}</span>
+	            <button class="delete-btn" type="button" onClick="deleteTask(this)">Delete</button>
+        	`
+
+
+
+		ul.insertAdjacentElement('beforeend', taskField);
+	}
+}
+
+taskList.forEach(task => {
+	if (task.description.length !== '') {
+		createNewTask(task.description);
+	}
+});
+
+
+function deleteTask(e) {
+	let parent = e.parentNode;      // get current task parent (li)
+	const deleteTarget = parent.querySelector('span').textContent;
+	taskList = taskList.filter(item => item.description !== deleteTarget);
+	updateLocalStorage();
+	ul.removeChild(parent);   // remove li from ul
+	// +удалить из списка
+
+}
+
+addBtn.addEventListener('click', () => {
+		createNewTask(inputField.value);
+		taskList.push(new Task(inputField.value));//add task to taskList
+		updateLocalStorage();//add task to local STORAGE!
+		inputField.value = "";//clear input field
+	}
+);
+
